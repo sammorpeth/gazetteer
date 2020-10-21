71b3c5d31a00348b6ab5b386826168efa3f9c23a
dosed91 <sammorpeth@gmail.com>
2020-10-21 11:44:09 +0100

    Added functionality for moving to the selected country. Added name to popup box for country.

--- libs/js/script.js
+++ libs/js/script.js
@@ -14,7 +14,8 @@ navigator.geolocation.getCurrentPosition(success);
 
 
 // declare map
-const mymap = L.map('mapid').setView([55.0006, 1.5], 6);
+const mymap = L.map('mapid').setView([55, 1.5], 6);
+
 
 // add tile layers
 var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -22,18 +23,24 @@ var OpenStreetMap_Mapnik = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
 
+// var polygon = L.polygon([
+//   [51.509, -0.08],
+//   [51.503, -0.06],
+//   [51.51, -0.047]
+// ]).addTo(mymap);
+// var marker = L.marker([51.5, -0.09]).addTo(mymap);
+
 // AJAX Calls
- 
+
+// Get country codes from decoded JSON 
 $.ajax({
   url: "libs/php/ISOCode.php",
   type: 'POST',
   dataType: 'json',
-  data: {
-   
-  },
+  
   success: function(result) {
 
-    
+    // Look through each country in the array and append them to the select element
     result['data'].forEach(country => {
       $('#selCountry').append($('<option>', {
         value: country['properties']['iso_a2'],
@@ -50,24 +57,41 @@ $.ajax({
   }
 }); 
 
+// 
 $('#submit').click(function() {
  
   $.ajax({
-    url: "libs/php/ISOCode.php",
+    url: "libs/php/restCountries.php",
     type: 'POST',
     dataType: 'json',
     data: {
-      
+      countryCode: $('#selCountry').val()
     },
     success: function(result) {
+      // set country's lat and lng
+      countryLat = result['data']['latlng'][0];
+      countryLng = result['data']['latlng'][1];
+      
+      // go to relevant lat and lng
+      mymap.flyTo([countryLat, countryLng], 6);
+      const popup = L.popup()
+            .setLatLng([countryLat, countryLng])
+            .setContent(`<div class="country-card">
+                         Name:${result['data']['name']}<br>
+                         Name:${result['data']['name']}<br>
+                         Name:${result['data']['name']}<br>
+                         Name:${result['data']['name']}<br>
+                         Name:${result['data']['name']}<br>
+                         Name:${result['data']['name']}<br>
+                         Name:${result['data']['name']}<br>
+                         Name:${result['data']['name']}<br>
+                         </div>`)
+            .openOn(mymap);
+      
+      // Should I make a function which builds up a template literal to construct the html for the country or just build it up
+      // within the AJAX call? 
 
-      const matchedCountry = result['data'].find(country => {
-        return country['properties']['iso_a2'] === $('#selCountry').val();
-      });
-      
-      
-      
-      
+
     },
     error: function(jqXHR, textStatus, errorThrown) {
       console.log(textStatus);
@@ -76,6 +100,5 @@ $('#submit').click(function() {
     }
   }); 
 });
-
 
 
--- libs/php/ISOCode.php
+++ libs/php/ISOCode.php
@@ -3,7 +3,7 @@
   $jsondata = file_get_contents('../json/countryBorders.geo.json');
   // convert the JSON into PHP variable
   $decode = json_decode($jsondata, true);
-
+  
 	$output['data'] = $decode['features'];
 
   echo json_encode($output);
